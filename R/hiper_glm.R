d8e5e415cdec2856f4f5f43d3d0497539b5bdde5
enp2116 <epotts5@jh.edu>
2025-01-29 21:14:30 -0500

    Outline functions with export, turn into S3 objects, roxygenize, build and install

---
+++ R/hiper_glm.R
@@ -0,0 +1,8 @@
+#' @export
+hiper_glm <- function(design, outcome) {
+  #TODO: maximize likelihood
+  warning("This function is yet to be implemented.")
+  hglm_out <- list()
+  class(hglm_out) <- "hglm"
+  return(hglm_out)
+}
